9ae67eba7262f4c1406bb5d9088b54bf53db15a3
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-03-06 22:15:34 -0500

    Review class 2

--- src/review2/CaptureTitle.java
+++ src/review2/CaptureTitle.java
@@ -1,17 +1,18 @@
-package review1;
+package review2;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LaunchApplication2 {
-
+public class CaptureTitle {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/Users/danielguerrero/Desktop/Selenium Review/Driver/chromedriver");
         WebDriver driver=new ChromeDriver();
-//driver.get("https://www.google.com/");
+driver.get("https://www.google.com/");
+driver.navigate().to("https://www.facebook.com/");
+        String pageTitle= driver.getTitle();
+        System.out.println("The title of the page is: "+pageTitle);
 
-        driver.navigate().to("https://www.google.com/");//this does the same thing as the get method
-        //the .to() method is nested inside the get method.
+
 
 
     }
--- src/review2/CaptureURL.java
+++ src/review2/CaptureURL.java
@@ -1,18 +1,15 @@
-package review1;
+package review2;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LaunchApplication2 {
-
+public class CaptureURL {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/Users/danielguerrero/Desktop/Selenium Review/Driver/chromedriver");
         WebDriver driver=new ChromeDriver();
-//driver.get("https://www.google.com/");
-
-        driver.navigate().to("https://www.google.com/");//this does the same thing as the get method
-        //the .to() method is nested inside the get method.
-
+        driver.get("https://www.facebook.com/");
+        String URL=driver.getCurrentUrl();
+        System.out.println("Page URL: "+URL);
 
     }
 }
